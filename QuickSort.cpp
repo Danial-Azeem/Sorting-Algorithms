f4e6aedb50b462260f4bd8eb7e35518d83e5b867
Danial Azeem <57601817+Danial-Azeem@users.noreply.github.com>
2019-11-11 22:42:43 +0500

    Add files via upload

---
+++ QuickSort.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+	template <class T>
+	int partition(T* array, int start, int end, bool asc){
+		T pivot = array[end];
+		int Pindex = start;
+		for (int i = start; i < end; i++)
+		{
+			if (asc){
+				if (array[i] <= pivot)
+				{
+					swap(array[i], array[Pindex]);
+					Pindex++;
+				}
+			} else {
+				if (array[i] >= pivot) 
+				{
+					swap(array[i], array[Pindex]);
+					Pindex++;
+				}
+			}
+		}
+		swap(array[Pindex], array[end]);
+		return Pindex;
+	}
+
+	template <class X>
+	void quickSort(X *array, int start, int end, bool asc) {
+		if (start < end) {
+			int Pindex = partition<X>(array, start, end, asc);
+			quickSort(array, start, Pindex - 1, asc);
+			quickSort(array, Pindex + 1, end, asc);
+		}
+	}
+
+	template<class Z>
+	void getArrTypeInp() {
+		system("cls");
+
+		int size;
+		Z *array;
+		cout << "Enter the number of elements: ";
+		cin >> size;
+		array = new Z[size];
+		cout << "Enter the elements: " << endl;
+
+		for (int i = 0; i < size; i++){
+			cin >> array[i];
+		}
+
+		system("cls");
+
+		cout << "Order," << endl;
+		cout << "1) Ascending" << endl;
+		cout << "2) Descending" << endl << endl;
+
+		int orderInp;
+		bool asc;
+		cout << "Input: " << endl;
+		cin >> orderInp;
+
+		switch (orderInp) {
+		case 1:
+			asc = true;
+			break;
+		case 2:
+			asc = false;
+			break;
+		}
+
+		system("CLS");
+		cout << "Unsorted Array: ";
+		
+		for (int i = 0; i < size; i++)
+			cout << array[i] << " ";
+
+		quickSort<Z>(array, 0, size - 1, asc);
+		
+		cout << "\nSorted Array: ";
+		for (int i = 0; i < size; i++)
+			cout << array[i] << " ";
+
+		cout << "\n";
+	};
+
+	void main(){
+		cout << "Enter array element type," << endl;
+		cout << "1) long" << endl;
+		cout << "2) float" << endl;
+		cout << "3) string" << endl;
+		cout << endl << endl << "Input: ";
+
+		int typeInp;
+
+		cin >> typeInp;
+
+		switch (typeInp) {
+			case 1:
+				getArrTypeInp<long>();
+				break;
+			case 2:
+				getArrTypeInp<float>();
+				break;
+			case 3:
+				getArrTypeInp<string>();
+				break;
+		}
+	}
